e08044c825aefa1f930e5467c838b324f0a8c793
RomanKosmyna <roma.kosmyna@gmail.com>
2025-12-16 19:49:16 +0200

    Add basic header, background wrapper. Remove default content

---
+++ src/components/MainWrapper.vue
@@ -0,0 +1,12 @@
+<template>
+    <main>
+        <slot />
+    </main>
+</template>
+
+<style>
+main {
+    width: 100%;
+    padding: 0 1.25rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import MainHeader from '@/components/MainHeader.vue';
 import TheWelcome from '../components/TheWelcome.vue'
+import BackgroundWrapper from '@/components/BackgroundWrapper.vue';
+import MainWrapper from '@/components/MainWrapper.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <BackgroundWrapper>
+    <MainWrapper>
+      <MainHeader />
+      <TheWelcome />
+    </MainWrapper>
+  </BackgroundWrapper>
 </template>
+
+<style>
+main {
+  width: 100%;
+  padding: 0 1.25rem;
+}
+</style>
